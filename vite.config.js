ebda4c8fc2d06e10f878a0fbf007c4476a32b7cb
Vanderson Alves <vanalvesdev@gmail.com>
2025-02-12 13:50:51 -0300

    add to github-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/vanddev/vite-project-game-project",
+  base: "/vite-project-game-project",
   plugins: [react()],
 })
